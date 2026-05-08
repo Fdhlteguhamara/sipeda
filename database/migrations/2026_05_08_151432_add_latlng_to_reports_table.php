6778686fd27a71d276c53808e33eacbfd9673606
Fdhlteguhamara <fadhil.teguh@mhs.itenas.ac.id>
2026-05-08 23:26:07 +0700

    Update 2026_05_08_151432_add_latlng_to_reports_table.php

--- database/migrations/2026_05_08_151432_add_latlng_to_reports_table.php
+++ database/migrations/2026_05_08_151432_add_latlng_to_reports_table.php
@@ -6,26 +6,25 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up()
+    public function up(): void
     {
         Schema::table('reports', function (Blueprint $table) {
 
             $table->string('latitude')->nullable();
+
             $table->string('longitude')->nullable();
 
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('reports', function (Blueprint $table) {
-            //
+
+            $table->dropColumn('latitude');
+
+            $table->dropColumn('longitude');
+
         });
     }
 };
